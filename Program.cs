9ea2168443b58135cef8f19524ac41faee781fc3
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-12 13:20:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ClassesExcercise.Math; //adds class from a separate file (ALT + ENTER)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person john = new Person(); //Person john = new Person(); //_ = new Person(); //var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
